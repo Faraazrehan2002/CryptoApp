2d4efa76dd12eac2fc7899dd2e07620a8b416879
Faraazrehan2002 <faraazrehan2002@gmail.com>
2024-09-29 11:29:58 -0500

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Faraaz Rehan Junaidi Mohammed on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
